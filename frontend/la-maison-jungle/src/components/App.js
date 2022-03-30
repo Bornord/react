7c13092539112a28e1cbed68f55c8917eb8e0371
Bornord <best.enguerran@gmail.com>
2022-03-30 10:01:58 +0200

    test

--- frontend/la-maison-jungle/src/components/App.js
+++ frontend/la-maison-jungle/src/components/App.js
@@ -1,9 +1,15 @@
 // import logo from '../images/logo.svg';
 import '../styles/App.css';
 import Banner from './Banner'
+import ShoppingList from './ShoppingList'
+
 
 function App() {
-  return <Banner/>
+  return 
+    <div>
+      <Banner/>
+      <ShoppingList/>
+    </div>
 }
 
 export default App
